d858b3e05a8679f1a8577f433f16b17bc1a81a56
Petrov Maxim <maxim.petrov@1ci.com>
2023-07-26 20:06:31 +0300

    stab

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -17,11 +17,13 @@ public class RegistrationTest {
     private String generateDate(int addDays, String pattern){
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
+
+
     @Test
     public void shouldRegisterByAccountNumber() {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Санкт-Петербург");
-        String currentDate = generateDate(4, "dd.MM.yyyy");
+        String currentDate = generateDate(3, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").sendKeys(currentDate);
         $("[data-test-id='name'] input").setValue("Артемов Артем");
